8e65d000d3702ff4d4028932668ca7609d050870
Olga Kroshkina <67290251+kroshkina-msft@users.noreply.github.com>
2022-02-22 13:18:40 -0800

    Master (#1)
    Little ASP.NET project

---
+++ OlgasLittleMvc3/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OlgasLittleMvc3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AwesomePost()
+        {
+            return View();
+        }
+    }
+}
